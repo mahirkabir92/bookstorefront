61b234a68da5a0021edf08439579c92d93d4686d
mahirkabir92 <141182263+mahirkabir92@users.noreply.github.com>
2023-10-31 11:19:56 -0400

    code for resubmission

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,15 +5,23 @@ import CreateBook from './pages/CreateBooks';
 import ShowBook from './pages/ShowBook';
 import EditBook from './pages/EditBook';
 import DeleteBook from './pages/DeleteBook';
+import AuthPage from './pages/AuthPage/AuthPage';
+import UserLogOut from './components/UserLogOut/UserLogOut';
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/books/create' element={<CreateBook />} />
-      <Route path='/books/details/:id' element={<ShowBook />} />
-      <Route path='/books/edit/:id' element={<EditBook />} />
-      <Route path='/books/delete/:id' element={<DeleteBook />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/books/create" element={<CreateBook />} />
+      <Route path="/books/details/:id" element={<ShowBook />} />
+      <Route path="/books/edit/:id" element={<EditBook />} />
+      <Route path="/books/delete/:id" element={<DeleteBook />} />
+
+      {/* Route for the authentication page */}
+      <Route path="/auth" element={<AuthPage />} />
+
+      {/* Route for the user logout */}
+      <Route path="/logout" element={<UserLogOut />} />
     </Routes>
   );
 };
---
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,23 @@
+// import { Link } from 'react-router-dom';
+// import * as userService from '../../utilities/users-service';
+
+// export default function NavBar({ user, setUser }) {
+//   function handleLogOut() {
+//     userService.logOut();
+//     setUser(null);
+//   }
+
+//   return (
+//     <nav>
+//       <Link to="/">Home</Link>
+//       &nbsp; | &nbsp;
+//       <Link to="/books">Add A Book</Link>
+//       &nbsp; | &nbsp;
+//       <Link to="/mybooks">My Books</Link>
+//       &nbsp;&nbsp;
+//       <span>Welcome, {user.name}</span>
+//       &nbsp;&nbsp;
+//       <Link to="" onClick={handleLogOut}>Log Out</Link>
+//     </nav>
+//   );
+// }
